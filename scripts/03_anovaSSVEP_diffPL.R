9c283a7feda72144a1a67b6da9d2b98c384b8f3a
Christian Panitz <christian.panitz@uni-leipzig.de>
2023-01-18 16:24:14 -0500

    Switch from anovaBF to generalTestBF for Bayesian ANOVAs
    - manually adding random slopes in the all models

--- scripts/03_anovaSSVEP_diffPL.R
+++ scripts/03_anovaSSVEP_diffPL.R
@@ -2,18 +2,26 @@
 # --- encoding: en_US.UTF-8
 # --- R version: 4.0.3 (2020-10-10) -- "Bunny-Wunnies Freak Out"
 # --- RStudio version: 2022.02.3
-# --- script version: August 2022
+# --- script version: December 2022
 # --- content: ANOVAs for ssVEP data (different processing pipelines)
 
 
-# Loading Packages, Setting Path & Random Number Generator Seed -----------
 
+# Header ------------------------------------------------------------------
+# For quicker computation of Bayesian ANOVA. From BayesFactor help:
+# "if TRUE use multiple cores through the doMC package. Unavailable on Windows."
+useMulticore = TRUE
+
+
+
+# Loading Packages, Setting Path & Random Number Generator Seed -----------
 # load required libraries
 library(here)
 library(rstatix)
 library(ez)
 library(BayesFactor)
 library(bayestestR)
+if (useMulticore) {library(doMC)}
 
 # set parent folder
 parentFolder <- here()
@@ -64,22 +72,54 @@ plotSSVEP <- ezPlot(
 
 # Bayesian ANOVA ----------------------------------------------------------
 
-# Bayesian Frequency x Modulation x lab ANOVA on z-standardized ssvep amplitudes
-set.seed(rngSeed); bayAnovaSSVEP <- anovaBF(
-                                    formula = ssvepZ ~ freq*mod*lab + part,
-                                    data = df,
-                                    whichRandom = "part",
-                                    whichModels = "all",
-                                    iterations = 10000
+# generTestBF is used instead of anovaBF to model random slopes of within-subject effects
+# this modeling approach is more in line with frequentist ANOVA
+# anovaBF models did not account for stable interivididual differences when estimating effects
+# which can lead to biased Bayes Factors in designs with 2+ within-subject factors
+# see van den Bergh et al. (2022, https://psyarxiv.com/fb8zn/)
+set.seed(rngSeed); bayAnovaSSVEP <- generalTestBF(
+  formula = ssvepZ ~ freq*mod*lab + part + part:freq + part:mod,
+  data = df,
+  whichRandom = c("part","part:freq","part:mod"),
+  whichModels = "all",
+  iterations = 10000,
+  neverExclude = c("part","part:freq","part:mod"),
+  multicore = useMulticore
 ); print(bayAnovaSSVEP)
 
 # extract 5 best models: sort all Bayes Factors in decending order, and get indices
 # of Factors in the Bayes ANOVA object; also return BF relative to null model
 # and relative to best model
-bfSorted <- sort(exp(bayAnovaSSVEP@bayesFactor[["bf"]]), decreasing = TRUE)
-match(bfSorted[1:5], exp(bayAnovaSSVEP@bayesFactor[["bf"]]))
+bfVec <- exp(bayAnovaSSVEP@bayesFactor[["bf"]])
+bfToNull <- bfVec/bfVec[length(bfVec)]
+bfSorted <- sort(bfToNull, decreasing = TRUE)
+match(bfSorted[1:5], bfToNull)
 bfSorted[1:5]
 bfSorted[1:5] / bfSorted[1]
 
 # compute Bayes Inclusion Factors
-bf_inclusion(bayAnovaSSVEP, match_models = FALSE)
+bfInc <- bf_inclusion(bayAnovaSSVEP, match_models = FALSE)
+bfInc
+bfInc$BF # to show exact values
+
+
+# use of anovaBF not recommended as of December 2022
+# # Bayesian Frequency x Modulation x lab ANOVA on z-standardized ssvep amplitudes
+# set.seed(rngSeed); bayAnovaSSVEP <- anovaBF(
+#                                     formula = ssvepZ ~ freq*mod*lab + part,
+#                                     data = df,
+#                                     whichRandom = "part",
+#                                     whichModels = "all",
+#                                     iterations = 10000
+# ); print(bayAnovaSSVEP)
+# 
+# # extract 5 best models: sort all Bayes Factors in decending order, and get indices
+# # of Factors in the Bayes ANOVA object; also return BF relative to null model
+# # and relative to best model
+# bfSorted <- sort(exp(bayAnovaSSVEP@bayesFactor[["bf"]]), decreasing = TRUE)
+# match(bfSorted[1:5], exp(bayAnovaSSVEP@bayesFactor[["bf"]]))
+# bfSorted[1:5]
+# bfSorted[1:5] / bfSorted[1]
+# 
+# # compute Bayes Inclusion Factors
+# bf_inclusion(bayAnovaSSVEP, match_models = FALSE)
